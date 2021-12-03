1df5746a32f3c0f7ef39cacdf72cc45954ecf15c
Phil Renaud <phil@riotindustries.com>
2021-12-02 23:03:12 -0500

    email ids to participant within events and basic consecutive tests

--- components/scheduler/src/init.spec.js
+++ components/scheduler/src/init.spec.js
@@ -4,7 +4,7 @@ describe("scheduler component", () => {
     cy.get("nylas-scheduler").should("exist");
   });
 
-  const slots_to_book = [
+  const basic_slots_to_book = [
     {
       selectionStatus: "selected",
       calendar_id: "abc123",
@@ -15,6 +15,158 @@ describe("scheduler component", () => {
     },
   ];
 
+  const hydrated_slots_to_book = [
+    {
+      selectionStatus: "selected",
+      event_title: "My event-hydrated title",
+      calendar_id: "abc123",
+      availability: "free",
+      available_calendars: ["thelonious@nylas.com"],
+      start_time: new Date(new Date().setHours(1, 0, 0, 0)),
+      end_time: new Date(new Date().setHours(3, 0, 0, 0)),
+    },
+  ];
+
+  const consecutive_slots_to_book = [
+    {
+      selectionStatus: "selected",
+      event_title: "My first meeting",
+      calendar_id: "abc123",
+      availability: "free",
+      available_calendars: ["thelonious@nylas.com", "booker@nylas.com"],
+      start_time: new Date(new Date().setHours(1, 0, 0, 0)),
+      end_time: new Date(new Date().setHours(3, 0, 0, 0)),
+    },
+    {
+      selectionStatus: "selected",
+      event_title: "My second meeting",
+      calendar_id: "abc123",
+      availability: "free",
+      available_calendars: ["miles@nylas.com"],
+      start_time: new Date(new Date().setHours(3, 0, 0, 0)),
+      end_time: new Date(new Date().setHours(5, 0, 0, 0)),
+    },
+  ];
+
+  // const consectuvie_options = [
+  //   [
+  //     {
+  //       "emails": [
+  //         "p1@nylas.com",
+  //         "p2@nylas.com"
+  //       ],
+  //       "end_time": "2021-12-01T18:45:00.000Z",
+  //       "start_time": "2021-12-01T18:30:00.000Z",
+  //       "event_title": "My Intro Meeting",
+  //       "event_description": "My Intro description",
+  //       "slot_size": 15,
+  //       "participants": [
+  //         "p1@nylas.com",
+  //         "p2@nylas.com"
+  //       ]
+  //     },
+  //     {
+  //       "emails": [
+  //         "hazik.a@nylas.com"
+  //       ],
+  //       "end_time": "2021-12-01T19:00:00.000Z",
+  //       "start_time": "2021-12-01T18:45:00.000Z",
+  //       "event_title": "My Closing Meeting",
+  //       "event_description": "My Closing description",
+  //       "slot_size": 15,
+  //       "participants": [
+  //         "p3@nylas.com"
+  //       ]
+  //     }
+  //   ]
+  // ]
+
+  const CONSECUTIVE_START_DATE = new Date("2021-11-01 GMT -0400");
+
+  describe.only("Consecutive Availability", () => {
+    beforeEach(() => {
+      cy.visit("/components/scheduler/src/index.html");
+    });
+
+    it("shows selectable consecutive options on load", () => {
+      cy.get("nylas-availability")
+        .as("availability")
+        .then((element) => {
+          const component = element[0];
+          component.show_weekends = false;
+          component.start_date = CONSECUTIVE_START_DATE;
+        });
+      cy.get("nylas-scheduler")
+        .as("scheduler")
+        .then((element) => {
+          cy.get("h2").should("contain", "Select an option");
+          cy.get("ul.timeslots li").should("have.length", 76);
+        });
+    });
+
+    it("is loosey-goosey with event ordering", () => {
+      cy.get("nylas-availability")
+        .as("availability")
+        .then((element) => {
+          const component = element[0];
+          component.show_weekends = false;
+          component.start_date = CONSECUTIVE_START_DATE;
+        });
+      cy.get("nylas-scheduler")
+        .as("scheduler")
+        .then((element) => {
+          // First event shows A then B
+          cy.get("ul.timeslots li:eq(0)")
+            .find(".sub-event:eq(0) h4")
+            .should("contain", "My Intro Meeting:");
+          cy.get("ul.timeslots li:eq(0)")
+            .find(".sub-event:eq(1) h4")
+            .should("contain", "My Intro Meeting2:");
+
+          // Second event shows B then A (participant A is busy at 9:00am)
+          cy.get("ul.timeslots li:eq(1)")
+            .find(".sub-event:eq(0) h4")
+            .should("contain", "My Intro Meeting2:");
+          cy.get("ul.timeslots li:eq(1)")
+            .find(".sub-event:eq(0) h4")
+            .should("contain", "My Intro Meeting:");
+        });
+    });
+
+    it.only("selects a timeslot on availability when a scheduler item is clicked", () => {
+      cy.get("nylas-availability")
+        .as("availability")
+        .then((element) => {
+          const component = element[0];
+          component.show_weekends = false;
+          component.start_date = CONSECUTIVE_START_DATE;
+        });
+      cy.intercept({
+        method: "POST",
+        url: "/middleware/calendars/availability/consecutive",
+      }).as("getConsecutive");
+
+      cy.wait("@getConsecutive").then(() => {
+        cy.get("nylas-scheduler")
+          .as("scheduler")
+          .then((element) => {
+            cy.get("ul.timeslots li:eq(0)").click();
+          });
+      });
+
+      cy.get("nylas-availability")
+        .as("availability")
+        .then((element) => {
+          const component = element[0];
+          component.show_weekends = false;
+          component.start_date = CONSECUTIVE_START_DATE;
+          cy.get(".slot.busy").should("have.length", 36);
+          cy.get(".slot.free").should("have.length", 124);
+          cy.get(".slot.selected").should("have.length", 2);
+        });
+    });
+  });
+
   describe("Inherits and passes properties", () => {
     it("has a default event title", () => {
       cy.get("nylas-availability").as("availability");
@@ -22,7 +174,7 @@ describe("scheduler component", () => {
         .as("scheduler")
         .then((element) => {
           const component = element[0];
-          component.slots_to_book = slots_to_book;
+          component.slots_to_book = basic_slots_to_book;
           cy.get("h3").contains("Meeting:");
         });
     });
@@ -32,22 +184,34 @@ describe("scheduler component", () => {
         $document.getElementsByTagName("nylas-scheduler")[0].remove();
         let newScheduler = $document.createElement("nylas-scheduler");
         newScheduler.id = "demo-scheduler";
-        newScheduler.slots_to_book = slots_to_book;
+        newScheduler.slots_to_book = basic_slots_to_book;
         $document.body.getElementsByTagName("main")[0].append(newScheduler);
       });
       cy.get("h3").contains("My Wonderful Event:");
     });
-    it("inherits event title from passed-property", () => {
+    it("inherits event title from passed-property if no event title", () => {
       cy.document().then(($document) => {
         $document.getElementsByTagName("nylas-scheduler")[0].remove();
         let newScheduler = $document.createElement("nylas-scheduler");
         newScheduler.id = "demo-scheduler";
         newScheduler.editor_id = "demo-schedule-editor";
         newScheduler.event_title = "Test-Passed Title";
-        newScheduler.slots_to_book = slots_to_book;
+        newScheduler.slots_to_book = basic_slots_to_book;
         $document.body.getElementsByTagName("main")[0].append(newScheduler);
       });
       cy.get("h3").contains("Test-Passed Title:");
+    });
+    it("inherits event title events array's object title", () => {
+      cy.document().then(($document) => {
+        $document.getElementsByTagName("nylas-scheduler")[0].remove();
+        let newScheduler = $document.createElement("nylas-scheduler");
+        newScheduler.id = "demo-scheduler";
+        newScheduler.editor_id = "demo-schedule-editor";
+        newScheduler.event_title = "Test-Passed Title";
+        newScheduler.slots_to_book = hydrated_slots_to_book;
+        $document.body.getElementsByTagName("main")[0].append(newScheduler);
+      });
+      cy.get("h3").contains("My event-hydrated title");
     });
   });
 
@@ -66,7 +230,7 @@ describe("scheduler component", () => {
         .as("scheduler")
         .then((element) => {
           const component = element[0];
-          component.slots_to_book = slots_to_book;
+          component.slots_to_book = basic_slots_to_book;
           cy.get("#custom-fields").should("exist");
         });
     });
@@ -76,7 +240,7 @@ describe("scheduler component", () => {
         .as("scheduler")
         .then((element) => {
           const component = element[0];
-          component.slots_to_book = slots_to_book;
+          component.slots_to_book = basic_slots_to_book;
           cy.get("#custom-fields strong").should("have.length", 2);
           cy.get("#custom-fields strong").contains("Email Address");
           cy.get("#custom-fields strong").contains("Your Name");
@@ -88,7 +252,7 @@ describe("scheduler component", () => {
         .as("scheduler")
         .then((element) => {
           const component = element[0];
-          component.slots_to_book = slots_to_book;
+          component.slots_to_book = basic_slots_to_book;
           cy.get("button.book").should("exist");
           cy.get("button.book").should("have.attr", "disabled");
           cy.get("#custom-fields label:eq(0) input").type("foo");
@@ -101,7 +265,7 @@ describe("scheduler component", () => {
         .as("scheduler")
         .then((element) => {
           const component = element[0];
-          component.slots_to_book = slots_to_book;
+          component.slots_to_book = basic_slots_to_book;
           cy.get("button.book").should("exist");
           cy.get("button.book").should("have.attr", "disabled");
           cy.get("#custom-fields label:eq(1) input").type("bar");
